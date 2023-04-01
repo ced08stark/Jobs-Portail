9be4d1c7d4af9af50f8ccc7b30823e3de50afa64
ced_stark08 <cedricngangue08@gmail.com>
2023-04-01 17:49:15 +0200

    change

--- pages/ErrorPage.jsx
+++ pages/ErrorPage.jsx
@@ -10,18 +10,16 @@ import "../assets/vendor/css/pages/page-misc.css";
 function ErrorPage() {
   return (
     <div>
-      <div class="container-xxl container-p-y">
-        <div class="misc-wrapper">
-          <h2 class="mb-2 mx-2">Page Not Found :</h2>
-          <p class="mb-4 mx-2">
+      <div className="container-xxl container-p-y">
+        <div className="misc-wrapper">
+          <h2 className="mb-2 mx-2">Page Not Found :</h2>
+          <p className="mb-4 mx-2">
             Oops! 😖 The requested URL was not found on this server.
           </p>
           <a href="index.html" class="btn btn-primary">
             Back to home
           </a>
-          <div class="mt-3">
-           
-          </div>
+          <div className="mt-3"></div>
         </div>
       </div>
     </div>
